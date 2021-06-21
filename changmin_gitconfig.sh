1f7dd01a0d8c43fd73fdda97a0673b362f065eb9
Changmin Yi <ycm93dg@gmail.com>
2021-06-21 18:31:55 +0900

    [feat] Add git message

--- changmin_gitconfig.sh
+++ changmin_gitconfig.sh
@@ -1,4 +1,6 @@
 git config --global user.name "Changmin Yi"
 git config --global user.email ycm93dg@gmail.com
 git config --global core.editor vim
+cp .gitmessage ~/
+git config --global commit.template ~/.gitmessage
 git config --list
