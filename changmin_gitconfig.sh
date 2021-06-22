deb15d748c99561951a1f70435b13e5cef6319c0
Changmin Yi <ycm93dg@gmail.com>
2021-06-22 15:37:29 +0900

    [feat] branch log beauti & commit message change
    - add git branch log all
    - edit git commit message

--- changmin_gitconfig.sh
+++ changmin_gitconfig.sh
@@ -4,3 +4,6 @@ git config --global core.editor vim
 cp .gitmessage ~/
 git config --global commit.template ~/.gitmessage
 git config --list
+git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+echo "  lg = !\"git lg1\"" >> ~/.gitconfig
